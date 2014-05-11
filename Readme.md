d4817d8b402feb9949c66f0a0db8745d3cf64f73
Peter Kiss <ypetya@gmail.com>
2014-05-11 14:21:00 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Collection of projects
+
+To download all the submodules :
+
+```shell
+git submodule init
+git submodule update
+```
+
+To add a new project :
+
+select it on the webpage and execute
+
+```shell
+install_submodule_from_clipboard.sh
+```
---
+++ install_submodule_from_clipboard.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This small bash script can install a git repo as a submodule
+# the only thing you have to do is select the repo url on bitbucket or github
+# and start the script.
+
+# require is a function defined in my .bash_ex file
+# it will install missing ubuntu packages
+require 'xclip'
+
+# selects the clipboard's content
+GIT_REPO=$(xclip -sel clip -o)
+# returns the end of the repo url
+DIR=${GIT_REPO##*/}
+
+if [ ! -d $DIR ] ; then
+  git submodule add $GIT_REPO $DIR
+fi
+
